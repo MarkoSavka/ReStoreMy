129c1d5e8ba37fee87fdd7323c63275ef0ad4d8a
Marko <m.savkalviv@gmail.com>
2024-07-29 01:00:31 +0300

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
         private readonly StoreContext _context;
